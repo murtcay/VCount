788844c4103cf6045a63e24b0f229e73ce641282
Murat ÇAY <murtcay@gmail.com>
2021-12-03 20:43:04 +0300

    get all countries with query selection and remove _id from response.

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -2,7 +2,14 @@ const Country = require('../models/Country');
 
 const getAllCountries = async (req, res) => {
   try {
-    const countries = await Country.find({});
+    const { region } = req.query;
+    const queryObject = {};
+    if(region) {
+      // regex for case insensitivity
+      queryObject.region = { $regex: region, $options: 'i' };
+    }
+
+    const countries = await Country.find(queryObject).select('-_id name region');
     res.status(200).json({ countries });
   } catch (error) {
     res.status(400).json({ msg: error.message });
